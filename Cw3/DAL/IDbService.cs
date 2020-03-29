c3b685165d494e568e24a71e5c2b151fe3a48842
popcat <bartek.nizio@gmail.com>
2020-03-29 17:44:32 +0200

    finished exercise

---
+++ Cw3/DAL/IDbService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Cw3
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ Cw3/DAL/MockDbService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Cw3
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student {IdStudent = 1, FirstName = "Jan", LastName = "Kowalski"},
+                new Student {IdStudent = 2, FirstName = "Anna", LastName = "Malewski"},
+                new Student {IdStudent = 3, FirstName = "Andrzej", LastName = "Andrzejewicz"}
+            };
+        }
+
+        public IEnumerable<Student> GetStudents() => _students;
+    }
+}
